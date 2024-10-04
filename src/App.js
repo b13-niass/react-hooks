a32757c141248478106b83b683020bf0686c4545
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-04 16:09:28 +0000

    useRef to get previous value

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import {useState, useEffect, useRef} from "react";
 
 function App() {
   const [name, setName] = useState("");
-  const renderCount = useRef(1);
-  const inputRef = useRef();
+  const prevName = useRef();
 
-  const focus = () =>{
-      inputRef.current.focus();
-      inputRef.current.value = "Some Value";
-  }
+  useEffect(() => {
+      prevName.current = name;
+  }, [name]);
 
   return (
     <div className="App">
-      <input ref={inputRef} value={name} onChange={e => setName(e.target.value)}/>
-      <div>My name is {name}</div>
-      <button onClick={focus}>Focus</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
+      <div>My name is {name} and it use to be {prevName.current}</div>
     </div>
   );
 }
