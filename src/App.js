f4a84bc8f7a115f09770c563c185a66c7b28346a
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-04 12:01:00 +0000

    useEffect to resize windows

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import './App.css';
 import {useState, useEffect} from "react";
 
 function App() {
-  const [resourceType, setResourceType] = useState('posts');
-  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    console.log("render")
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    }
 
     useEffect(() =>{
-        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-            .then(response => response.json())
-            .then(async json => await setItems(json))
-  },[resourceType]);
+      window.addEventListener("resize", handleResize)
+      return () => window.removeEventListener("resize", handleResize);
+  },[]);
   return (
     <div className="App">
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={() => setResourceType('comments')}>Comments</button>
-      <h1>{resourceType}</h1>
-        {items.map(item => {
-            return <pre key={item.id}>{JSON.stringify(item)}</pre>
-        })}
+        {windowWidth}
     </div>
   );
 }
