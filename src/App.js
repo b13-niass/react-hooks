46c82249ce2ce543ad3382171ff64bb9f0dcd945
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-10-04 10:53:47 +0000

    debut exemple avec useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+  const [resourceType, setResourceType] = useState('posts');
+    console.log("render")
+
+    useEffect(() =>{
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+            .then(response => response.json())
+            .then(json => console.log(json))
+  },[resourceType]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
+      <h1>{resourceType}</h1>
     </div>
   );
 }
